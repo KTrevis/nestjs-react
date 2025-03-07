4043e2764153dcd1f34d60adb8d8c9b003111d26
KTrevis <treviskevin77@gmail.com>
2025-03-07 18:01:13 +0100

    added not found page

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,11 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router"
 import Index from './Index.tsx'
+import NotFound from './NotFound.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
   	<Routes>
 		<Route path="/" element={<Index />} />
+		<Route path="*" element={<NotFound />} />
 	</Routes>
   </BrowserRouter>,
 )
